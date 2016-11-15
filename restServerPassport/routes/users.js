84ff8c6daef1248c918f7513fecbd59e5d609d7e
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-11-15 15:22:09 -0700

    Updated users.js

--- restServerPassport/routes/users.js
+++ restServerPassport/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var passport = require('passport');
+var User = require('../models/user');
+var Verify = require('./verify');
 
 module.exports = router;
