894697380966b3b1f4bb8dad8aebaf8a3db02092
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-11-17 17:26:57 -0700

    Template Engines Part 2 with comments

--- practice/server/app10.js
+++ practice/server/app10.js
@@ -16,12 +16,17 @@ app.get('/contact', function (req, res) {
     res.sendFile(__dirname + '/contact.html');
 });
 
+// We can use the name in the url to template to profile page
 app.get('/profile/:name', function (req, res) {
+    // Data to pass to the template
     var data = {
         age: 17,
-        job: "Coder"
+        job: "Coder",
+        hobbies: ['coding', 'gaming', 'more coding']
     };
 
+    // render the template and send the necesarry data
     res.render('profile', {person: req.params.name, data: data});
 });
+
 app.listen(3000);
