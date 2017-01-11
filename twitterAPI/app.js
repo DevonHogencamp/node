b68365d9889b47b732c442562b97ff22827c6d65
Devon Hogencamp <devhogen1@gmail.com>
2017-01-11 15:20:22 -0700

    added search for tweet followers installed query string and async

--- twitterAPI/app.js
+++ twitterAPI/app.js
@@ -1,6 +1,7 @@
 // All of our requires
 var url = require('url');
 var express = require('express');
+var queryString = require('querystring');
 var authenticator = require('./authenticator.js');
 var config = require('./config.json');
 
@@ -12,29 +13,29 @@ app.use(require('cookie-parser')());
 // This is handeled by our authenticator.js
 app.get('/auth/twitter', authenticator.redirectToTwitterLogin);
 
-app.get(url.parse(config.oauth_callback).path, function (req, res) {
-    authenticator.authenticate(req, res, function (err) {
+app.get(url.parse(config.oauth_callback).path, function(req, res) {
+    authenticator.authenticate(req, res, function(err) {
         if (err) {
             console.log(err);
             res.sendStatus(401);
-        }
-        else {
+        } else {
             res.send('Authentication Successful!');
         }
     });
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-app.get('/tweet', function (req, res) {
+app.get('/tweet', function(req, res) {
     if (!req.cookies.access_token || !req.cookies.access_token_secret) {
         return res.sendStatus(401);
     }
+
     authenticator.post('https://api.twitter.com/1.1/statuses/update.json', req.cookies.access_token, req.cookies.access_token_secret, {
-        status : 'This tweet was made using Node.JS again for the second time!!!'
-    }, function (error, data) {
+        status: 'This tweet was made using Node.JS 123456'
+    }, function(error, data) {
         if (error) {
             return res.status(400).send(error);
         }
@@ -42,7 +43,39 @@ app.get('/tweet', function (req, res) {
     });
 });
 
-app.listen(config.port, function () {
+app.get('/search', function(req, res) {
+    if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+        return res.sendStatus(401);
+    }
+
+    authenticator.get('https://api.twitter.com/1.1/search/tweets.json?' + queryString.stringify({q:'Trump'}), req.cookies.access_token, req.cookies.access_token_secret, function(error, data) {
+        if (error) {
+            return res.status(400).send(error);
+        }
+        res.send(data);
+    });
+});
+
+app.get('/followers', function (req, res) {
+    if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+        return res.sendStatus(401);
+    }
+
+    var url = 'https://api.twitter.com/1.1/friends/list.json';
+
+    if (req.query.cursor) {
+        url += '?' + queryString.stringify({cursor : req.query.cursor});
+    }
+
+    authenticator.get(url, req.cookies.access_token, req.cookies.access_token_secret, function(error, data) {
+        if (error) {
+            return res.status(400).send(error);
+        }
+        res.send(data);
+    });
+});
+
+app.listen(config.port, function() {
     console.log("Server running on port " + config.port);
 
     console.log('OAuth callback: ' + url.parse(config.oauth_callback).hostname + url.parse(config.oauth_callback).path);
